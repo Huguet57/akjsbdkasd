a854be5b642862854888c0fe060220412c417ca3
Sergey Potekhin <sergey@pimlico.io>
2025-05-03 02:39:45 +0100

    Fix ts

--- src/lib/toSimple7702SmartAccount.ts
+++ src/lib/toSimple7702SmartAccount.ts
@@ -64,6 +64,7 @@ export async function toSimple7702SmartAccount(
 
   return toSmartAccount({
     authorization: {
+      // @ts-ignore
       account:  localOwner,
       address: implementation
     },
