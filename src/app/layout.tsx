ef1ab24d16ddd9282bbb6de935a8d394f1fd36cd
Garvit Khatri <garvitdelhi@gmail.com>
2025-05-03 03:01:48 +0100

    add co

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
